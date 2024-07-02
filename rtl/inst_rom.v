95245899085afb3e48ad2242f1dce3059e7ffee3
Library Cat <1120803547@qq.com>
2024-07-03 00:10:29 +0800

    chapter 4 : ori

---
+++ rtl/inst_rom.v
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////
+// Description: 指令存储器
+//////////////////////////////////////////////////////////////////////
+
+`include "defines.v"
+
+module inst_rom(
+	input wire						ce,
+	input wire[`InstAddrBus]		addr,
+	output reg[`InstBus]			inst
+);
+
+	// 定义一个数组，大小是 InstMemNum，元素宽度是 InstBus
+	reg[`InstBus]  inst_mem[0:`InstMemNum-1];
+
+	// 使用文件 inst_rom.data 初始化指令存储器
+	initial $readmemh ( "inst_rom.data", inst_mem );
+
+	// 当复位信号无效时，依据输入的地址，给出指令存储器 ROM 中对应的元素
+	always @ (*) begin
+		if (ce == `ChipDisable) begin
+			inst <= `ZeroWord;
+		end else begin
+			inst <= inst_mem[addr[`InstMemNumLog2+1:2]];
+		end
+	end
+
+endmodule
---
+++ rtl/openmips_min_sopc_tb.v
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////
+// Description: openmips_min_sopcµÄtestbench
+//////////////////////////////////////////////////////////////////////
+
+`include "defines.v"
+`timescale 1ns/1ps
+
+module openmips_min_sopc_tb();
+
+	parameter PEROID = 20;
+
+	reg     CLOCK_50;
+	reg     rst;
+		
+	initial begin
+		CLOCK_50 = 1'b0;
+		forever #(PEROID/2) CLOCK_50 = ~CLOCK_50;
+	end
+
+	initial begin
+		rst = `RstEnable;
+		#115 rst = `RstDisable;
+		#(PEROID*50) $stop;
+	end
+
+	openmips_min_sopc openmips_min_sopc0(
+		.clk(CLOCK_50),
+		.rst(rst)
+	);
+
+endmodule
